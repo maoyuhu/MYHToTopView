292b27864284e2a701503b64c27e7cfc80b4b025
maoyuhu <13162766101@163.com>
2016-06-28 23:46:18 +0800

    初始化框架
    点击视图最上方 的状态栏   回到最顶处

---
+++ MYHToTopViewDemo/MYHToTopViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYHToTopViewDemo
+//
+//  Created by myh on 16/6/28.
+//  Copyright © 2016年 myh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MYHToTopViewDemo/MYHToTopViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MYHToTopViewDemo
+//
+//  Created by myh on 16/6/28.
+//  Copyright © 2016年 myh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
